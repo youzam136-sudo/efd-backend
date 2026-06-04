46a07fe76538d633625c54f081074b1332998d77
youzam136-sudo <youzam136@gmail.com>
2026-06-04 15:20:16 +0900

    add cloudinary upload provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,4 +6,14 @@ export default () => ({
     enabled: true,
     resolve: './src/plugins/cms-editor',
   },
+  'upload': {
+    config: {
+      provider: '@strapi/provider-upload-cloudinary',
+      providerOptions: {
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key: process.env.CLOUDINARY_KEY,
+        api_secret: process.env.CLOUDINARY_SECRET,
+      },
+    },
+  },
 });
